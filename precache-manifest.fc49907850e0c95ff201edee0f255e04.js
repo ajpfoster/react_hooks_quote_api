53a871237a8fb2b4c07e9462737a842cc3cf5770
Ash Foster <ajp.foster@gmail.com>
2020-08-05 03:37:34 +1200

    Updates

---
+++ precache-manifest.fc49907850e0c95ff201edee0f255e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b31515b1ae08dfdb619611e357a383",
+    "url": "/react_hooks_quote_api/index.html"
+  },
+  {
+    "revision": "6cf7ef6b15e558efa6bb",
+    "url": "/react_hooks_quote_api/static/css/main.b92e2e7a.chunk.css"
+  },
+  {
+    "revision": "09159d15d81c19d491ed",
+    "url": "/react_hooks_quote_api/static/js/2.b401195b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hooks_quote_api/static/js/2.b401195b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf7ef6b15e558efa6bb",
+    "url": "/react_hooks_quote_api/static/js/main.10f760f6.chunk.js"
+  },
+  {
+    "revision": "629092ef0254dd840ac1",
+    "url": "/react_hooks_quote_api/static/js/runtime-main.c9a1bff7.js"
+  }
+]);
